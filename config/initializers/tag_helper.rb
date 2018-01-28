b8e3e7e78710906291094c86265293d76e4029c3
Jean-Baptiste Vilain <jbvilain@gmail.com>
2018-01-29 00:52:57 +0100

    temp commit

--- config/initializers/tag_helper.rb
+++ config/initializers/tag_helper.rb
@@ -7,20 +7,25 @@ module ActionView
         attr_reader :id, :elements, :edit
 
         [:h1, :h2, :h3, :h4, :h5, :h6, :p, :span, :div].each do |t|
-          define_method(t) do |id, cl = nil|
-            html_opts = edit ? {class: "sb-#{t} #{cl}", data: {id: id}} : (cl ? {class: cl} : {})
+          define_method(t) do |id, html_opts = {}|
+            html_opts.merge!({class: "sb-#{t} #{html_opts[:class]}", data: {id: id}}) if edit
             content_tag(t, (elements.dig(id, "text") || "Texte à modifier"), html_opts)
           end
         end
 
         def initialize(id, elts, edit_mode)
           @id = id
-          @elements = Hash[elts.map {|e| [e["id"], e]}]
+          @elements = Hash[elts.map {|e| [e["id"], e.except("id")]}]
           @edit = edit_mode
         end
 
-        def img(id)
-          elements.dig(id, "src") || "/default.jpg"
+        def img(id, html_opts = {})
+          if edit
+            content_tag(:div, content_tag(:img, nil, (elements[id] || {"src" => "/default.jpg"}).merge(html_opts)),
+                        {class: "sb-img #{html_opts[:class]}", data: {id: id}})
+          else
+            content_tag(:img, nil, (elements[id] || {"src" => "/default.jpg"}).merge(html_opts))
+          end
         end
 
         def lnk(id)
