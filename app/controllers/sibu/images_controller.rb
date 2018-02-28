3b8fc26e28ec9b3c1d189be8724550a4835cd55c
Jean-Baptiste Vilain <jbvilain@gmail.com>
2018-02-28 14:53:46 +0100

    0.1.27 fix img upload on site entity

--- app/controllers/sibu/images_controller.rb
+++ app/controllers/sibu/images_controller.rb
@@ -17,15 +17,15 @@ module Sibu
       @image = Sibu::Image.new(image_params)
       if @image.save
         if @page_id && @section_id && @element_id && @size
-          page = Sibu::Page.find(@page_id)
+          entity = @entity_type == 'site' ? @site : Sibu::Page.find(@page_id)
           ids = (@section_id.split('|') + @element_id.split('|')).uniq[0...-1]
-          elt = page.update_element(*ids, {"id" => @img_id, "src" => @image.file_url(@size.to_sym), "alt" => @image.alt})
+          elt = entity.update_element(*ids, {"id" => @img_id, "src" => @image.file_url(@size.to_sym), "alt" => @image.alt})
           if elt.nil?
             msg = {alert: "Une erreur s'est produite lors de la mise à jour de l'image."}
           else
             msg = {notice: "L'image a bien été mise à jour."}
           end
-          redirect_to site_page_edit_content_path(@site.id, page.id), msg
+          redirect_to site_page_edit_content_path(@site.id, @page_id), msg
         else
           redirect_to site_images_url(@image.site_id), notice: "L'image a bien été téléchargée."
         end
@@ -55,6 +55,7 @@ module Sibu
 
     def set_edition_context
       @page_id = params[:page_id]
+      @entity_type = params[:entity_type]
       @section_id = params[:section_id]
       @element_id = params[:element_id]
       @img_id = params[:img_id]
