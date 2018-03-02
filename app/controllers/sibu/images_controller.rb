44f85c89d249bc0b50fd2a10437a833aca34b460
Jean-Baptiste Vilain <jbvilain@gmail.com>
2018-03-02 17:31:29 +0100

    0.2.1 images admin actions

--- app/controllers/sibu/images_controller.rb
+++ app/controllers/sibu/images_controller.rb
@@ -2,7 +2,8 @@ require_dependency "sibu/application_controller"
 
 module Sibu
   class ImagesController < ApplicationController
-    before_action :set_site, only: [:index, :new, :create, :edit]
+    before_action :set_site, only: [:index, :new, :create, :edit, :update, :destroy]
+    before_action :set_image, only: [:edit, :update, :destroy]
     before_action :set_edition_context, only: [:new, :create]
 
     def index
@@ -16,7 +17,7 @@ module Sibu
     def create
       @image = Sibu::Image.new(image_params)
       if @image.save
-        if @page_id && @section_id && @element_id && @size
+        if !@page_id.blank? && !@section_id.blank? && !@element_id.blank? && !@size.blank?
           entity = @entity_type == 'site' ? @site : Sibu::Page.find(@page_id)
           ids = (@section_id.split('|') + @element_id.split('|')).uniq[0...-1]
           elt = entity.update_element(*ids, {"id" => @img_id, "src" => @image.file_url(@size.to_sym), "alt" => @image.alt})
@@ -42,15 +43,27 @@ module Sibu
     end
 
     def update
+      if @image.update(image_params)
+        redirect_to site_images_url(@site.id), notice: "L'image a bien été mise à jour."
+      else
+        flash.now[:alert] = "Une erreur s'est produite lors de l'enregistrement de l'image."
+        render :index
+      end
     end
 
     def destroy
+      @image.destroy
+      redirect_to site_images_url(@site.id), notice: "L'image a bien été supprimée."
     end
 
     private
 
     def set_site
       @site = Sibu::Site.find(params[:site_id])
+    end
+
+    def set_image
+      @image = Sibu::Image.find(params[:id])
     end
 
     def set_edition_context
