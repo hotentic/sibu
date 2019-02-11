fdb75ff7d778b29d80b97803871b6e3320b78cfc
Jean-Baptiste Vilain <jbvilain@gmail.com>
2019-02-11 16:27:30 +0100

    0.7.3 config max doc size

--- app/models/concerns/sibu/document_uploader.rb
+++ app/models/concerns/sibu/document_uploader.rb
@@ -2,7 +2,7 @@ class Sibu::DocumentUploader < Shrine
   plugin :validation_helpers
 
   Attacher.validate do
-    validate_max_size 10*1024*1024, message: "est trop volumineux (10Mo maximum)"
+    validate_max_size (Rails.application.config.sibu[:max_doc_size] || 10)*1024*1024, message: "est trop volumineux (#{Rails.application.config.sibu[:max_doc_size] || 10}Mo maximum)"
     # validate_mime_type_inclusion %w[application/pdf]
   end
 end
