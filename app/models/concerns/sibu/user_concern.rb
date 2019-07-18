bdcb8fe990b4cbeeb87fe9c249d551da4d9e7fbe
Jean-Baptiste Vilain <jbvilain@gmail.com>
2019-07-18 13:07:40 +0100

    0.8.3 Order docs and images by desc update date

--- app/models/concerns/sibu/user_concern.rb
+++ app/models/concerns/sibu/user_concern.rb
@@ -3,7 +3,8 @@ module Sibu
     include ActiveSupport::Concern
 
     def for_user(usr)
-      Rails.application.config.sibu[:multi_user] ? where(user_id: [nil, usr.id]) : all
+      entities = (Rails.application.config.sibu[:multi_user] ? where(user_id: [nil, usr.id]) : all)
+      entities.order(updated_at: :desc)
     end
   end
 end
